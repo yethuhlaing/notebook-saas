e166328c4b0ab13139ed8940e753d83466c58373
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-03-05 10:53:43 +0200

    Complete NavBar Feature

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "../../components/ui/button";
+
+export function Navbar() {
+  return (
+    <nav className="border-b bg-background h-[10vh] flex items-center">
+        <div className="container flex items-center justify-between">
+          <Link href="/">
+            <h1 className="font-bold text-3xl">Noteworthy</h1>
+          </Link>
+          <div className="flex items-center gap-x-5">
+            <ThemeToggle />
+            <div className="flex items-center gap-x-5">
+              <Button>Sign in</Button>
+              <Button variant="secondary">Sign Up</Button>
+            </div>
+          </div>
+        </div>
+    </nav>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { ThemeToggle } from './components/ThemeToggle'
+export default function Home() {
+  return(
+    <div>
+      <ThemeToggle />
+    </div>
+  )
+}
