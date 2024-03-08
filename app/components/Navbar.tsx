a4c9062952746ff21300188de33226383df2134a
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-03-08 09:10:58 +0200

    Complete Kinde Authentication

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import { ThemeToggle } from "./ThemeToggle";
 import { Button } from "../../components/ui/button";
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
+export async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession()
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
         <div className="container flex items-center justify-between">
@@ -11,10 +14,22 @@ export function Navbar() {
           </Link>
           <div className="flex items-center gap-x-5">
             <ThemeToggle />
-            <div className="flex items-center gap-x-5">
-              <Button>Sign in</Button>
-              <Button variant="secondary">Sign Up</Button>
-            </div>
+            { await isAuthenticated() ? 
+              (
+                <LogoutLink>
+                  <Button>Log out</Button>
+                </LogoutLink>
+              ) : (
+              <div className="flex items-center gap-x-5">
+                <LoginLink>
+                  <Button>Sign in</Button>
+                </LoginLink>
+                <RegisterLink>
+                  <Button variant="secondary">Sign Up</Button>
+                </RegisterLink>
+              </div>
+              )
+            }
           </div>
         </div>
     </nav>
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage(){
+    return(
+        <div>Hello word from dadshboard</div>
+    )
+}
