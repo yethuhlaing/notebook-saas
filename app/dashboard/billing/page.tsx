7b16762b95e3d39929f4b4c8d58d0cf8f8f9d265
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2024-06-14 01:42:37 +0300

    Change Production Variables

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -62,7 +62,7 @@ export default async function BillingPage(){
         }
         const subscriptionUrl = await getStripeSession({
             customerId: dbUser.stripeCustomerId,
-            domainUrl: 'http://localhost:3000',
+            domainUrl: process.env.NODE_ENV === "production" ? process.env.PRODUCTION_URL as string : 'http://localhost:3000',
             priceId: process.env.STRIPE_PRICE_ID as string,
         })
         return redirect(subscriptionUrl);
